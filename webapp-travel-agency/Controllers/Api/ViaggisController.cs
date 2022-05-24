6b5abb5cf318c2e3428fe2c863c57698616b08ee
LucaVanossi <vanossi.luca13@gmail.com>
2022-05-24 22:29:34 +0200

    Milestone 1 / 2

---
+++ webapp-travel-agency/Controllers/Api/ViaggisController.cs
@@ -0,0 +1,27 @@
+﻿using webapp_travel_agency.Data;
+using webapp_travel_agency.Models;
+using webapp_travel_agency.Utils;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapp_travel_agency.Controllers.Api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ViaggisController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<Viaggio> viaggi = new List<Viaggio>();
+
+            using (BlogContext db = new BlogContext())
+            {
+                viaggi = db.Viaggi.ToList<Viaggio>();
+            }
+
+            return Ok(viaggi);
+        }
+
+
+    }
+}
